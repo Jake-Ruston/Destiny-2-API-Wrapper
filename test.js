5e68fe181ab3dea6543392ec731b1ddca6f2a926
Jake Ruston <rjake@hotmail.co.uk>
2017-10-25 19:15:47 +0100

    had token lol

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
+const { TOKEN } = require('config');
 const destiny = require('.');
-const client = new destiny.Client('');
+const client = new destiny.Client(TOKEN);
 
 client.getProfile('PushyParachute4', '1').then(data => {
   console.log(data);
