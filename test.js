ec6f6069acce170288f7251ed2cd89dc17e14b49
Jake Ruston <rjake@hotmail.co.uk>
2017-10-25 19:12:09 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const destiny = require('.');
-const client = new destiny.Client('c060f847214f4a7eb4d8db139dabd7f4');
+const client = new destiny.Client('');
 
 client.getProfile('PushyParachute4', '1').then(data => {
   console.log(data);
